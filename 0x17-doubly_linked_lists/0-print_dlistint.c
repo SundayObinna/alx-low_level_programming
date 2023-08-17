922131c26a6d9220c7cde2031a4d1810e66065e2
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-16 23:37:38 -0700

    0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Prints all elements of a list
+ * @h: Elements
+ * Return: Node count
+ *
+ * Code by 0xOneBeing
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_count++;
+	}
+
+	return (node_count);
+}
+
