970177b4c19daf49f044a9f1ce71e7bc4a94c1d5
Sunday Oruwhone <sundayobinna4jobs@gmail.com>
2023-08-21 22:00:22 -0700

    Advanced Task 1

---
+++ 0x18-dynamic_libraries/100-prints_hello.c
@@ -0,0 +1,12 @@
+#include "adv.h"
+
+/**
+ * prints_hello - Fxn prints 'Hello' then a new line.
+ *
+ * Code by 0xOneBeing
+ */
+
+void prints_hello(void)
+{
+	printf("Hello\n");
+}
---
+++ 0x18-dynamic_libraries/adv.h
@@ -0,0 +1,10 @@
+#ifndef _ADV_H_
+#define _ADV_H_
+
+#include <stdio.h>
+
+#include <stdio.h>
+
+void prints_hello(void);
+
+#endif /* ADV.H */
